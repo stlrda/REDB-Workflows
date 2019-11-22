426fc48a66eb08c5a4e46e642e98b67e85a2037c
Jonathan Leek <jonathanleek@gmail.com>
2019-11-22 12:21:32 -0600

    fixing git ignore

--- python/download_city_file.py
+++ python/download_city_file.py
@@ -6,14 +6,15 @@ import wget
 import datetime
 import io
 
-def download_city_file(file_url,destination_directory):
-    print('Downloading file: ',file_url)
-    print('To location: ',destination_directory)
+
+def download_city_file(file_url, destination_directory):
+    print('Downloading file: ', file_url)
+    print('To location: ', destination_directory)
     start_time = datetime.datetime.now()
-    print('Starting at: ',start_time.strftime("%a, %b %d, %Y %I:%M:%S %p"))
-    wget.download(file_url,destination_directory)
+    print('Starting at: ', start_time.strftime("%a, %b %d, %Y %I:%M:%S %p"))
+    wget.download(file_url, destination_directory)
     end_time = datetime.datetime.now()
-    print('Finished at: ',end_time.strftime("%I:%M:%S %p"))
+    print('Finished at: ', end_time.strftime("%I:%M:%S %p"))
 
 #
 # for item in city_files:
