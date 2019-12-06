8915be4fc0eae05d93cb391078d67c03475acddd
Jonathan Leek <jonathanleek@gmail.com>
2019-12-06 13:30:42 -0600

    Adding TODOs to retreive_city_datasoures

--- python/Scripts/retrieve_city_datasources.py
+++ python/Scripts/retrieve_city_datasources.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import urllib.request
 
-working_directory = '/Users/jonathanleek/Desktop/working'
+working_directory = '/Users/jonathanleek/Desktop/working' # TODO modify retrieve_city_datasources to utilize s3 bucket
 
 def retrieve_city_datasources():
     df = pd.read_csv(working_directory+'/city_datasource_list.csv')
@@ -9,6 +9,6 @@ def retrieve_city_datasources():
     targets = list(set(targets))
     for target in targets:
         print("Retieving " + target)
-        urllib.request.urlretrieve(target, working_directory + "/" + target.rsplit('/', 1)[-1])
+        urllib.request.urlretrieve(target, working_directory + "/" + target.rsplit('/', 1)[-1]) # TODO is there a better way to retrieve file name?
         print("Downloaded "+target.rsplit('/', 1)[-1])
-retrieve_city_datasources()
+#retrieve_city_datasources()
