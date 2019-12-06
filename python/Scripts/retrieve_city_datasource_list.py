2973d2c016903291f3fd1192e500d1b6981f732b
Jonathan Leek <jonathanleek@gmail.com>
2019-12-06 14:36:26 -0600

    Adding TODOs to unzip_directory.py

--- python/Scripts/retrieve_city_datasource_list.py
+++ python/Scripts/retrieve_city_datasource_list.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-city_datasource_list_url = 'https://raw.githubusercontent.com/stlrda/redb_python/master/reference/redb_source_databases_all-info.csv'
+city_datasource_list_url = 'https://raw.githubusercontent.com/stlrda/redb_python/master/reference/redb_source_databases_all-info.csv' # TODO point this to the same file in /config, and delete the one in reference
 working_directory = '/Users/jonathanleek/Desktop/working' # TODO modify retrieve_city_datasource_list to utilize s3 bucket
 
 def retrieve_city_datasource_list():
