c4145f2c0ff94aac55fadfa462ccbd9426b8ae42
Jonathan Leek <jonathanleek@gmail.com>
2019-12-06 13:28:51 -0600

    Adding TODOs to retreive_city_datasource_list

--- python/Scripts/retrieve_city_datasource_list.py
+++ python/Scripts/retrieve_city_datasource_list.py
@@ -1,10 +1,10 @@
 import urllib.request
 
 city_datasource_list_url = 'https://raw.githubusercontent.com/stlrda/redb_python/master/reference/redb_source_databases_all-info.csv'
-working_directory = '/Users/jonathanleek/Desktop/working'
+working_directory = '/Users/jonathanleek/Desktop/working' # TODO modify retrieve_city_datasource_list to utilize s3 bucket
 
 def retrieve_city_datasource_list():
     print('Downloading list of city datasources...')
     urllib.request.urlretrieve(city_datasource_list_url, working_directory+'/city_datasource_list.csv')
 
-retrieve_city_datasource_list()
+#retrieve_city_datasource_list()
