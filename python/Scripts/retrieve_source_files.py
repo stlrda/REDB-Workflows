cbfb82dc5a772496f9c962a46aa8c484ccfb71f5
dso1222 <dayne.okamura@daugherty.com>
2019-12-09 13:43:51 -0600

    Takes URL, Downloads to Temp, Upload to S3

--- python/Scripts/retrieve_source_files.py
+++ python/Scripts/retrieve_source_files.py
@@ -1,17 +1,43 @@
-import pandas as pd
-import io
-import requests
+import logging
 import boto3
+from botocore.exceptions import ClientError
+import requests
 import urllib.request
-import wget
+import tempfile
+import io
+import pandas as pd
 
-url="https://raw.githubusercontent.com/stlrda/redb_python/master/config/redb_source_databases_urls.csv"
-s=requests.get(url).content
-working_directory = '/Users/jonathanleek/Desktop/working/'
-def retreive_source_files():
-    targets=pd.read_csv(io.StringIO(s.decode('utf-8')))
-    print(targets)
-    for index, row in targets.iterrows():
-        print("Retrieving" + row['Zip File Name'])
-        wget.download(row['Direct URL'], working_directory + row['Zip File Name'])
-retreive_source_files()
+def upload_file(url, bucket, profile='default'):
+    
+    """Upload a file to an S3 bucket
+    :param url: URL Path to files
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    
+    #Create list of url to pull zip from
+    try:
+        s = requests.get(url).content
+        df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+        targets = df['Direct URL']
+        targets = list(set(targets))
+    except:
+        return (f'ERROR - check URL:  {url}')
+
+    #If no profile specified use default
+    boto3.setup_default_session(profile_name=profile) 
+    
+    #iterate through list of url and download zip
+    for target in targets:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            print("Retieving " + target)
+            urllib.request.urlretrieve(target, tmpdirname + "/" + target.rsplit('/', 1)[-1]) # TODO is there a better way to retrieve file name?
+            print("Downloaded "+target.rsplit('/', 1)[-1])
+            s3_client = boto3.client('s3')
+            try:
+                s3_client.upload_file(tmpdirname + "/" + target.rsplit('/', 1)[-1], bucket, target.rsplit('/', 1)[-1])
+            except ClientError as e:
+                logging.error(e)
+                return False
+            return True
