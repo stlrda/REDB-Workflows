46434dddf953ac70e120ba084b984ddd03939356
Moreland <TIM0416@daugherty.com>
2020-04-14 10:46:35 -0500

    Made paths backwards compatible and cleaned up Python code

--- python/Scripts/transfer_to_s3.py
+++ python/Scripts/transfer_to_s3.py
@@ -13,89 +13,91 @@ import pandas as pd
 from colorama import Fore, Style
 from botocore.exceptions import ClientError
 
-# Relative (for Dockerized Airflow testing)
-SOURCES_CSV = "resources/redb_source_databases_all-info.csv"
 
-# Relative for local testing
-# SOURCES_CSV = "../resources/redb_source_databases_all-info.csv"
-
+SOURCES_CSV = "resources/redb_source_databases_all-info.csv" # ! Relative (for Dockerized Airflow testing)
+#SOURCES_CSV = "../resources/redb_source_databases_all-info.csv" # ! Relative for local testing.
 SOURCES_DATAFRAME = pd.read_csv(SOURCES_CSV)
 TIMESTAMP = '{:%Y-%m-%d %H:%M:%S}'.format
 sources_visited = []
 
-def upload_file(file, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file: Path for file to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
+def get_list_of_files(directory):
+    
+    """ 
+    For the given path, return a List of all files in the directory tree.
     """
 
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        # Forward slash is for UNIX/Linus systems only. Change to backslash for Windows.
-        object_name = re.sub(r'.*/', '', file)
+    items_in_directory = os.listdir(directory)
+    all_files = list()
 
-    s3_client = boto3.client('s3', aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-                      aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"])
-
-    # Upload the file
-    try:
-        print(object_name)
-        print(f'\nUploading {file} to {bucket} in s3...')
-        s3_client.upload_file(file, bucket, object_name)
-        print(f'{object_name} successfully uploaded to {bucket} in s3.')
-
-    except Exception as e:
-        print(e)
-        logging.error(e)
-        return False
-
-    return True
-
-
-# ? For the given path, get the List of all files in the directory tree 
-def getListOfFiles(directory):
-    
-    # create a list of file and sub directories 
-    # names in the given directory 
-    listOfFile = os.listdir(directory)
-    allFiles = list()
-
-    # Iterate over all the entries
-    for entry in listOfFile:
+    # Iterate over all of the items.
+    for item in items_in_directory:
 
         # Create full path
-        fullPath = os.path.join(directory, entry)
+        fullPath = os.path.join(directory, item)
 
-        # If entry is a directory then get the list of files in this directory 
+        # If item is a directory then get the list of files in this directory.
         if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
+            all_files = all_files + get_list_of_files(fullPath)
         else:
-            allFiles.append(fullPath)
+            all_files.append(fullPath)
                 
-    return allFiles 
+    return all_files 
 
 
 def unzip(files, targetDirectory):
+    """
+    Unzips a List of files to specified directory.
 
+    :param: files -> List of absolute filepaths.
+    :param: targetDirectory -> absolute path for result of unzipped file.
+    """
     for file in files:
 
         if (file.endswith(".zip")):
 
             with ZipFile(file, 'r') as zipObj:
                 zipObj.extractall(targetDirectory)
-            os.remove(f'{file}')
+            os.remove(file)
+    
     
     if (".zip" in [name[-4:] for name in os.listdir(targetDirectory)]):
-        unzip(getListOfFiles(targetDirectory), targetDirectory)
+        unzip(get_list_of_files(targetDirectory), targetDirectory)
     else:
+        # Prints a preview of file extentions located in targetDirectory.
         print([name[-4:] for name in os.listdir(targetDirectory)])
 
 
-def tempfile_to_s3():
+def upload_file(file, bucket):
+    """Upload a file to an S3 bucket
 
+    :param file: Path for file to upload
+    :param bucket: Bucket to upload to
+    :return: True if file was uploaded, else False
+    """
+    
+    # Removes all text prior to final forward slash (UNIX) or final backslash (Windows).
+    object_name = re.sub(r'.*(/|\\)', '', file)
+
+    s3_client = boto3.client('s3', aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                      aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"])
+
+    # Upload the file
+    try:
+        print(f'\nUploading {file} to {bucket} in s3...')
+        s3_client.upload_file(file, bucket, object_name)
+        print(f'{Fore.GREEN}{object_name} successfully uploaded to {bucket} in s3.{Style.RESET_ALL}')
+
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
+
+
+def tempfile_to_s3():
+    """
+      Creates temporary folder that is then used to unzip and upload source files.
+    """
     for index, row in SOURCES_DATAFRAME.iterrows():
 
         link_name = row["Link Name"]
@@ -104,27 +106,26 @@ def tempfile_to_s3():
 
         # ? strings for stdout
         downloading = f'\nDownloading {file_name}- {link_name} \nFrom: {url}'
-        success = f'\nDownload for {file_name}: {Fore.GREEN}Successful.{Style.RESET_ALL}'
+        success = f'\nDownload for {file_name}: {Fore.GREEN}SUCCESSFUL{Style.RESET_ALL}'
 
         if (url not in sources_visited):
             with tempfile.TemporaryDirectory() as tmp:
+
+                # ? Destination for temporary file.
+                path = os.path.join(tmp, file_name)
+
                 try:
                     print(downloading)
                     sources_visited.append(url)
-                    # ! bar must be set to None to comply with Airflow default settings
-                    wget.download(url, f'{tmp}/{file_name}', bar=None) 
-                    print(success)
-                    unzip(getListOfFiles(f'{tmp}'), f'{tmp}')
-                    print(os.listdir(tmp))
+                    wget.download(url, path, bar=None) # ! Use bar=None to avoid errors.
+                    print(success + " @ " + path)
+                    unzip(get_list_of_files(tmp), tmp)
                     
                 except Exception as err:
-                    time = TIMESTAMP(dt.datetime.now())
-                    message = f'\nDownload for {file_name}: {Fore.RED}Unsuccessful:{Style.RESET_ALL} {err}'
+                    #time = TIMESTAMP(dt.datetime.now())
+                    message = f'\nDownload for {file_name}: {Fore.RED}UNSUCCESSFUL:{Style.RESET_ALL} {err}'
                     print(message)
-                    logging.error(time + message)
 
                 for file in os.listdir(tmp):
-                    # Forward slash is for UNIX/Linus systems only. Change to backslash for Windows.
-                    file = f'{tmp}/{file}'
-                    print(file)
-                    upload_file(file, 'redb-test')
+                    path = os.path.join(tmp, file)
+                    upload_file(path, 'redb-test')
--- python/dags/sources_to_s3.py
+++ python/dags/sources_to_s3.py
@@ -11,18 +11,18 @@ from airflow.operators.python_operator import PythonOperator
 sys.path.append(".")
 from Scripts.transfer_to_s3 import tempfile_to_s3
 from Scripts.unzip_s3_objects import unzip
+from credentials import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
+os.environ["AWS_SECRET_ACCESS_KEY"] = AWS_SECRET_ACCESS_KEY
 
 default_args = {
     'owner': 'airflow',
-    'start_date': dt.datetime(2020, 4, 13, 18, 00, 00),
+    'start_date': dt.datetime(2020, 4, 14, 15, 25, 00),
     'concurrency': 1,
     'retries': 0
 }
 
-# https://crontab.guru/#10_*_*_*_*
 
 with DAG('SourcesToS3',
          default_args=default_args,
