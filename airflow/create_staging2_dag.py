c4f625653f6805cd1e3e0005c035f6956fb594ab
Brummund <SWB1230@daugherty.com>
2020-04-15 16:01:51 -0500

    create dag for creating staging2

---
+++ airflow/create_staging2_dag.py
@@ -0,0 +1,62 @@
+# Standard Library
+import os
+import sys
+from datetime import datetime, timedelta
+
+# Third Party
+from airflow import DAG
+from airflow.operators.python_operator import python_operator
+from airflow.hooks.base_hook import BaseHook
+
+# Make python folder a module
+sys.path.append(".")
+from python.create_staging2_schema import create_schema, create_tables
+
+
+# Connect to Amazon Aurora Postgres database using Airflow
+CONN = BaseHook.get_connection("redb")
+BUCKET = CONN.conn_id
+HOST = CONN.host
+LOGIN = CONN.login
+PASSWORD = CONN.password
+PORT = CONN.port
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2020, 4, 15, 03, 00, 00),
+    "concurrency": 1,
+    "retries": 3
+}
+
+
+dag = DAG(
+    "staging_2",
+    default_arg=default_args,
+    schedule_interval=timedelta(days=1)
+)
+
+create_schema = PythonOperator(
+    task_id="create_schema",
+    python_callable=create_schema,
+    op_kwargs-{
+        "database": BUCKET,
+        "host": HOST,
+        "username": LOGIN,
+        "password": PASSWORD,
+        "port": PORT
+    }
+)
+
+create_tables = PythonOperator(
+    task_id="create_tables",
+    python_callable=create_tables,
+    op_kwargs-{
+        "database": BUCKET,
+        "host": HOST,
+        "username": LOGIN,
+        "password": PASSWORD,
+        "port": PORT
+    }
+)
+
+create_schema >> create_tables
--- python/create_staging2_schema.py
+++ python/create_staging2_schema.py
@@ -1,24 +1,64 @@
 import psycopg2
 
 
-# Create conncetion to database
-connection = psycopg2.connect(
-    f"""
-    host={redb_host}
-    database={redb_dbname}
-    port={redb_port}
-    user={redb_username}
-    password={redb_password}
-    """
-)
+def create_schema(database, host, username, password, port):
+    # Create conncetion to database
+    connection = psycopg2.connect(
+        f"""
+        host={host}
+        database={database}
+        port={port}
+        user={username}
+        password={password}
+        """
+    )
 
-# Create cursor to execute sql statements/queries
-cursor = connection.cursor()
-
-# Read sql file to get sql statements/queries
-file = open("./sql/staging_2/create_staging2_schema.sql", "r")
-create_staging2_schema = file.read()
-file.close()
+    # Create cursor to execute sql statements/queries
+    cursor = connection.cursor()
 
 
-cursor.execute(create_staging2_schema)
+    # Read sql file to get sql statement that will create the staging_2 schema
+    schema_file = open("./sql/staging_2/create_staging2_schema.sql", "r")
+    create_staging2_schema = schema_file.read()
+    schema_file.close()
+
+    # Execute sql statement to create staging_2 schema
+    cursor.execute(create_staging2_schema)
+
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def create_tables(database, host, username, password, port):
+    # Create conncetion to database
+    connection = psycopg2.connect(
+        f"""
+        host={host}
+        database={database}
+        port={port}
+        user={username}
+        password={password}
+        """
+    )
+
+    # Create cursor to execute sql statements/queries
+    cursor = connection.cursor()
+
+
+    # Read sql file to get sql statements that will create the tables within staging_2 schema
+    create_tables_file = open("./sql/staging_2/create_tables.sql", "r")
+    create_tables = create_tables_file.read()
+    create_tables_file.close()
+
+    # split sql
+    statements = create_tables.split(";")
+
+    # Loop through the create table commands to pull out each individual sql statement
+    # Execute sql statements to create tables
+    for statement in statements:
+        cursor.execute(statement)
+
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
