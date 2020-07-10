13c6be79276e657e71601d78d437b5ade7400e15
Moreland <TIM0416@daugherty.com>
2020-07-10 11:03:28 -0500

    Merged, now committing  theo/dev

--- sql/inserts/legal_entity.sql
+++ sql/inserts/legal_entity.sql
@@ -1,38 +1,65 @@
-BEGIN;
-CREATE OR REPLACE TEMPORARY VIEW owner_address_ids AS (
-    SELECT
-           ha."address_id"
-           ,pp."OwnerName"
-           ,pp."OwnerName2"
-            ,ha.street_address AS str
-            ,pp."OwnerAddr" AS ownaddr
-    FROM history.address ha
-    INNER JOIN staging_1_2.prcl_prcl pp
-        ON ha."street_address" = pp."OwnerAddr"
-);
+----------------------------insert new legal entities----------------------------
+CREATE OR REPLACE FUNCTION core.new_legal_entity()
+RETURNS void AS $$
+BEGIN
 
-WITH present_legal_entities AS (
-    SELECT
-        CONCAT(legal_entity_name, legal_entity_secondary_name) AS entity
-    FROM history.legal_entity
-    --WHERE current_flag = True
-)
-INSERT INTO history.legal_entity
+WITH GET_ADDRESS_ID AS 
 	(
-	address_id
-	,legal_entity_name
-	,legal_entity_secondary_name
+	WITH NEW_LEGAL_ENTITIES AS 
+		(
+		SELECT DISTINCT CURRENT_WEEK."OwnerName"
+			, CURRENT_WEEK."OwnerName2"
+			, CURRENT_WEEK."OwnerAddr"
+			, CURRENT_WEEK."OwnerCity"
+			, CURRENT_WEEK."OwnerState"
+			, CURRENT_WEEK."OwnerCountry"
+			, CURRENT_WEEK."OwnerZIP"
+		FROM "staging_1"."prcl_prcl" AS CURRENT_WEEK
+		LEFT JOIN "staging_2"."prcl_prcl" AS PREVIOUS_WEEK
+		ON CONCAT(CURRENT_WEEK."OwnerName", CURRENT_WEEK."OwnerName2", CURRENT_WEEK."OwnerAddr", CURRENT_WEEK."OwnerCity", CURRENT_WEEK."OwnerState", CURRENT_WEEK."OwnerCountry", CURRENT_WEEK."OwnerZIP") 
+			= CONCAT(PREVIOUS_WEEK."OwnerName", PREVIOUS_WEEK."OwnerName2", PREVIOUS_WEEK."OwnerAddr", PREVIOUS_WEEK."OwnerCity", PREVIOUS_WEEK."OwnerState", PREVIOUS_WEEK."OwnerCountry", PREVIOUS_WEEK."OwnerZIP")
+		WHERE PREVIOUS_WEEK."ParcelId" IS NULL
+		)
+	SELECT DISTINCT "OwnerName"
+		, "OwnerName2"
+		, "OwnerAddr"
+		, "address"."address_id"
+	FROM NEW_LEGAL_ENTITIES
+	JOIN "core"."address"
+	ON CONCAT(NEW_LEGAL_ENTITIES."OwnerAddr", NEW_LEGAL_ENTITIES."OwnerCity", NEW_LEGAL_ENTITIES."OwnerState", NEW_LEGAL_ENTITIES."OwnerCountry", NEW_LEGAL_ENTITIES."OwnerZIP") 
+		= CONCAT("street_address", "city", "state", "country", "zip")
 	)
-	SELECT DISTINCT
-        "address_id"
-        ,"OwnerName"
-        ,"OwnerName2"
-	FROM owner_address_ids oai
-	WHERE NOT EXISTS(
-	    SELECT
-            entity
-	    FROM present_legal_entities ple
-	    WHERE CONCAT(oai."OwnerName", oai."OwnerName2") = ple.entity
-        )
-    RETURNING *;
-COMMIT;
+INSERT INTO "core"."legal_entity"(
+	"legal_entity_address"
+	, "legal_entity_name"
+	, "legal_entity_secondary_name"
+	, "address_id"
+	, "create_date"
+	, "current_flag"
+	, "removed_flag"
+	--, "etl_job"
+	, "update_date"
+	)
+SELECT "OwnerAddr"
+	, "OwnerName"
+	, "OwnerName2"
+	, "address_id"
+	, CURRENT_DATE
+	, TRUE
+	, FALSE
+	, CURRENT_DATE
+FROM GET_ADDRESS_ID
+ON CONFLICT (COALESCE("legal_entity_address", 'NULL_ADDRESS')
+	, COALESCE("legal_entity_name", 'NULL_NAME_1')
+	, COALESCE("legal_entity_secondary_name", 'NULL_NAME_2')
+	, "address_id")
+	DO UPDATE
+SET "current_flag" = TRUE
+	, "removed_flag" = FALSE
+	, "update_date" = CURRENT_DATE;
+
+END;
+$$
+LANGUAGE plpgsql;
+-------------------------
+SELECT new_legal_entity();
--- sql/inserts/neighborhood.sql
+++ sql/inserts/neighborhood.sql
@@ -1,17 +1,39 @@
-INSERT INTO history.neighborhood
-    (
-     neighborhood_name
-     ,county_id
-    )
-    SELECT DISTINCT
-                    pp."Nbrhd"
-    ,(SELECT county_id FROM history.county WHERE county.county_name = 'Saint Louis City County')
-    FROM "staging_1"."prcl_prcl" pp
-    WHERE NOT EXISTS(
-        SELECT
-               neighborhood_name
-        FROM history.neighborhood hn
-        WHERE pp."Nbrhd" = hn."neighborhood_name"
-            AND hn.current_flag = True
-        )
-    RETURNING *;
+-------------------------Insert new Neighborhoods-------------------------
+CREATE OR REPLACE FUNCTION core.new_neighborhoods()
+RETURNS void AS $$
+BEGIN
+
+WITH NEW_NEIGHBORHOODS AS
+	(
+	SELECT DISTINCT CURRENT_WEEK."Nbrhd"
+	FROM "staging_1"."prcl_prcl" AS CURRENT_WEEK
+	LEFT JOIN "staging_2"."prcl_prcl" AS PREVIOUS_WEEK
+	ON CURRENT_WEEK."Nbrhd" = PREVIOUS_WEEK."Nbrhd"
+	WHERE PREVIOUS_WEEK."Nbrhd" IS NULL
+	ORDER BY CURRENT_WEEK."Nbrhd"
+	)
+INSERT INTO "core"."neighborhood"("neighborhood_name"
+	, "county_id"
+	, "create_date"
+	, "current_flag"
+	, "removed_flag"
+	--, "etl_job"
+	, "update_date"
+	)
+SELECT "Nbrhd"
+	, '10001'
+	, CURRENT_DATE
+	, TRUE
+	, FALSE
+	, CURRENT_DATE
+FROM NEW_NEIGHBORHOODS
+ON CONFLICT ON CONSTRAINT UC_Neighborhood DO UPDATE
+SET "current_flag" = TRUE
+	, "removed_flag" = FALSE
+	, "update_date" = CURRENT_DATE;
+
+END;
+$$
+LANGUAGE plpgsql;
+-------------------------
+SELECT core.new_neighborhoods();
